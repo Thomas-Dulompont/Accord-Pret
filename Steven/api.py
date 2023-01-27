6ae2a7ee8f0e191790db13ef65169928f5d777ab
StevenDelval <asteven59220@free.fr>
2023-01-27 11:32:42 +0100

    Change get in post

--- Steven/api.py
+++ Steven/api.py
@@ -3,23 +3,12 @@ import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
-
+from pydantic import BaseModel
 pickle_in = open('./model_XGBoost.pkl', 'rb') 
 model_XGBoost = pickle.load(pickle_in)
 
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:5500",
-    "http://localhost:8000",
-    "https://localhost",
-    "https://localhost:8080",
-    "https://localhost:5500",
-    "https://localhost:8000",
-    "127.0.0.1:8000",
     "http://127.0.0.1:5500",
 ]
 
@@ -36,33 +25,23 @@ middleware = [
 
 app = FastAPI(middleware=middleware)
 
+class Request(BaseModel):
+    State : str
+    BankState: str
+    Term : float
+    NoEmp : float
+    NewExist : str
+    UrbanRural :str
+    LowDoc : int
+    GrAppv : float
+    have_franchise : int
+    sector : str
+    in_recession: int
 
-@app.get("/predict/")
-def predict(State : str,
-    BankState: str,
-    Term : float,
-    NoEmp : float,
-    NewExist : str,
-    UrbanRural :str,
-    LowDoc : int,
-    GrAppv : float,
-    have_franchise : int,
-    sector : str,
-    in_recession: int):
 
-    data={
-        "State" : State,
-        "BankState": BankState,
-        "Term" : Term,
-        "NoEmp" : NoEmp,
-        "NewExist" : NewExist,
-        "UrbanRural" :UrbanRural,
-        "LowDoc" : LowDoc,
-        "GrAppv" : GrAppv,
-        "have_franchise" : have_franchise,
-        "sector" : sector,
-        "in_recession": in_recession
-    }
+@app.post("/predict/")
+def predict(data:Request):
+
     pred=pd.DataFrame(dict(data),index=[0])
     class_pred = model_XGBoost.predict(pred)[0]
     if class_pred == 0 :
