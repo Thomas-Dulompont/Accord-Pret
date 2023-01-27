ecb2ff0d3c910686b202b3a028724ed5156929c7
StevenDelval <asteven59220@free.fr>
2023-01-27 14:56:56 +0100

    form

--- AccordPret/model/forms.py
+++ AccordPret/model/forms.py
@@ -3,28 +3,28 @@ from django import forms
 etats = [('IN', 'IN'),('CT', 'CT'),('FL', 'FL'),('NC', 'NC'),('IL', 'IL'),('OK', 'OK'),('AR', 'AR'),('MN', 'MN'),('CA', 'CA'),('SC', 'SC'),('TX', 'TX'),('LA', 'LA'),('IA', 'IA'),('TN', 'TN'),('MS', 'MS'),('OH', 'OH'),('MD', 'MD'),('VA', 'VA'),('MA', 'MA'),('PA', 'PA'),('OR', 'OR'),('ME', 'ME'),('KS', 'KS'),('MI', 'MI'),('AK', 'AK'),('WA', 'WA'),('CO', 'CO'),('WY', 'WY'),('UT', 'UT'),('MO', 'MO'),('AZ', 'AZ'),('ID', 'ID'),('RI', 'RI'),('NJ', 'NJ'),('NH', 'NH'),('NM', 'NM'),('NV', 'NV'),('NY', 'NY'),('ND', 'ND'),('VT', 'VT'),('WI', 'WI'),('MT', 'MT'),('AL', 'AL'),('GA', 'GA'),('KY', 'KY'),('NE', 'NE'),('WV', 'WV'),('SD', 'SD'),('DE', 'DE'),('DC', 'DC'),('HI', 'HI')]
 
 class ModelForm(forms.Form):
-    Name = forms.CharField(label="Nom de votre entreprise")
-    City = forms.CharField(label="Ville")
-    Zip = forms.IntegerField(label="Code Postal", min_value=10000, max_value=99999)
+    Name = forms.CharField(label="Nom de votre entreprise",required=False)
+    City = forms.CharField(label="Ville",required=False)
+    Zip = forms.IntegerField(label="Code Postal", min_value=10000, max_value=99999,required=False)
     State = forms.CharField(label="Etat", widget=forms.Select(choices=etats))
     NoEmp = forms.IntegerField(label="Nombre d'employés", min_value=1, max_value=9999)
-    FranchiseCode = forms.BooleanField(label="Franchisé ?")
+    FranchiseCode = forms.CharField(label="Franchisé ?", widget=forms.Select(choices=[(1,"oui"),(0,"non")]))
     NAICS = forms.IntegerField(label="Code NAICS (Laisser vide si vous n'en avez pas)")
-    NewExist = forms.BooleanField(label="Plus de 2 ans de creation",widget=forms.CheckboxInput())
+    NewExist = forms.CharField(label="A t-elle plus de 2ans ?", widget=forms.Select(choices=[(1,"oui"),(0,"non")]))
     UrbanRural = forms.CharField(label="Zone Rurale ou Urbaine")
    
    
 
     # Objectifs
-    CreateJob = forms.IntegerField(label="Nombre de poste créés", min_value=1, max_value=5621)
-    RetainedJob = forms.IntegerField(label="Nombre de poste sauvegardés", min_value=1, max_value=9500)
+    CreateJob = forms.IntegerField(label="Nombre de poste créés", min_value=1, max_value=5621,required=False)
+    RetainedJob = forms.IntegerField(label="Nombre de poste sauvegardés", min_value=1, max_value=9500,required=False)
 
     # Infos crédit
-    Bank = forms.CharField(label="Nom de votre banque")
+    Bank = forms.CharField(label="Nom de votre banque",required=False)
     BankState = forms.CharField(label="Etat", widget=forms.Select(choices=etats))
 
-    LowDoc = forms.BooleanField(label="Suivez vous le programme 'LowDoc' ?")
-    RevLineCr = forms.BooleanField(label="Crédit renouvelable")
+    LowDoc = forms.CharField(label="Suivez vous le programme 'LowDoc' ?", widget=forms.Select(choices=[(1,"oui"),(0,"non")]))
+    RevLineCr = forms.BooleanField(label="Crédit renouvelable",required=False)
     Term = forms.IntegerField(label="Nombre d'échéances en mois", min_value=1, max_value=569)
     GrAppv = forms.IntegerField(label="Montant accordé par la banque", min_value=1000, max_value=5472000)
-    in_recession =forms.BooleanField(label="En recession ?")
+    in_recession = forms.CharField(label="En recession ?", widget=forms.Select(choices=[(1,"oui"),(0,"non")]))
