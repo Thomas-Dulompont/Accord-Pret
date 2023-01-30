8c5ad1358b38fafe8abc3600cb8f17d951ed9f4b
Thomas-Dulompont <thomasdulompont.1@gmail.com>
2023-01-30 15:26:36 +0100

    ee

--- Django/model/views.py
+++ Django/model/views.py
@@ -3,7 +3,7 @@ from . import forms,functions
 import requests
 import json
 
-url="http://127.0.0.1:5000/predict/"
+url="https://api-accord-pret.onrender.com/predict/"
 
 def home_view(request):
     if request.method == 'POST':
